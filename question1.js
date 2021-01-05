d9a8232d9967cac5f09f7873221a1ae62e3d9ce3
TaibaBugammaz <76516938+TaibaBugammaz@users.noreply.github.com>
2021-01-05 23:14:30 +0300

    Done

--- question1.js
+++ question1.js
@@ -1,1 +1,44 @@
 // قم بحل السؤال الأول هنا
+
+let pets=["cat","dog","bird","rabbit","fish"];
+
+console.log(pets[4]);
+
+pets.pop()
+
+
+let mypet=["horse"];
+
+pets.push(mypet)
+
+
+
+
+
+// for (let index = 0; index < pets.length; index++) {
+//     const element = pets[index];
+//     console.log(element);
+// }
+
+pets.splice(2,2,`fish`)
+
+
+
+let cats= {
+    name: "acat",
+    age: "7",
+    type: "siamese",
+}
+
+cats.emoji= "🐱";
+
+console.log(cats.age);
+console.log(cats)
+
+
+
+
+
+
+
+
